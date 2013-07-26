d267a6d9d539ac6024f92eb2a89a3bd3280b2335
Erika Nana <enana@hawaii.edu>
2013-07-25 15:19:06 -1000

    Initial commit

---
+++ src/com/multithreading/IncomingHandlerCallback.java
@@ -0,0 +1,14 @@
+package com.multithreading;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class IncomingHandlerCallback implements Handler.Callback {
+
+	@Override
+	public boolean handleMessage(Message arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
